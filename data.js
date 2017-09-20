58abacdf85f9fede1af7ad058f8d4c4c9bd9d5b8
WEdwards <WEdwards@sk.ibm.com>
2017-09-20 17:19:44 +0200

    updated template

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 module.exports = {
 	// this data is passed by the app to the Mustache templating engine
 	codingTools : [
-		{ node: "Node.js" },
-		{ express: "Express.js" },
-		{ Mustache: "Mustache.js" }
+		{ "name" : "Node.js" },
+		{ "name" : "Express.js" },
+		{ "name" : "Mustache.js" }
 	]
 
 }
