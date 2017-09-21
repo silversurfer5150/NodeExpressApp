23d0bfcc2a9e4acd6ef34048238145b2a1317680
WEdwards <WEdwards@sk.ibm.com>
2017-09-21 11:06:12 +0200

    uploaded images, altered template and css

--- data.js
+++ data.js
@@ -1,9 +1,18 @@
 module.exports = {
 	// this data is passed by the app to the Mustache templating engine
 	codingTools : [
-		{ "name" : "Node.js" },
-		{ "name" : "Express.js" },
-		{ "name" : "Mustache.js" }
+		{ "name" : "Node.js",
+		"description" : "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient.",
+		"image" : "nodejsLogo.svg"
+		},
+		{ "name" : "Express.js", 
+		"description" : "Express is a fast, unopinionated, minimalist web framework for Node.js",
+		"image" : "expressLogo.png"
+		},
+		{ "name" : "Mustache.js",
+		"description" : "mustache.js - Logic-less {{mustache}} templates with JavaScript",
+		"image" : "mustacheLogo.png"
+		},
 	]
 
 }
